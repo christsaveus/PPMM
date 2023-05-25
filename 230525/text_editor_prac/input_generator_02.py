43231e37b2fa67aef704e8bd1eb30d65c0f04b37
Hanhyeok_Jang <gksgur1493@gmail.com>
2023-05-25 11:59:41 +0900

    [230525]

---
+++ 230525/text_editor_prac/input_generator_02.py
@@ -0,0 +1,34 @@
+import re
+import xml.etree.ElementTree as ET
+import csv
+
+# Parse the XML file
+tree = ET.parse('depth02_template_b.feb')
+root = tree.getroot()
+
+# Find all SolidDomain elements
+solid_domains = root.findall('.//MeshDomains/SolidDomain')
+
+# Open the CSV file
+with open('sample.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)  # Skip the header row
+
+    for j, row in enumerate(reader):
+        for i, domain in enumerate(solid_domains[1:]):
+            if i >= 49:
+                continue
+            domain.set('mat', str(row[i]))
+
+        # Save modified XML to a new file
+        new_file_name = f"depth02_template_b_{j}.feb"
+
+        # Add XML declaration manually
+        xml_declaration = '<?xml version="1.0" encoding="ISO-8859-1"?>\n'
+
+        # Convert modified XML tree to string
+        modified_xml_str = ET.tostring(root, encoding='ISO-8859-1').decode('ISO-8859-1')
+
+        # Write modified XML to file
+        with open(new_file_name, 'w', encoding='ISO-8859-1') as output_file:
+            output_file.write(modified_xml_str)
